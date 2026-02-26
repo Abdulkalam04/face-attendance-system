4f209acba625b2c3a2aa8d9252434112b7f71ba8
Abdul <fceproject9@gmial.com>
2026-02-26 21:25:16 +0530

    Deployment update

--- backend/app.py
+++ backend/app.py
@@ -1512,4 +1512,4 @@ def attendance_status():
 
 # ---------------- START SERVER ----------------
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
